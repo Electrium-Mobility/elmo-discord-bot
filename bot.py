cffc616bda6d73447fda8f02b8b0d8c7cb843773
Faisal-Shahbaz <faisalsshahbaz1@gmail.com>
2024-01-09 19:17:10 -0500

    Rework for slash commands

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord 
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord import app_commands
 import os
 
 # Load environment variables from .env file
@@ -10,33 +11,39 @@ def run_discord_bot():
     TOKEN = os.getenv('DISCORD_TOKEN')  # Retrieve token from environment variable
     intents = discord.Intents.default()
     intents.members = True
-    intents.message_content = True
     description = "Electrium's discord bot"
-    bot = commands.Bot(command_prefix='&', description=description, intents=intents)
+    bot = commands.Bot(command_prefix='/', description=description, intents=intents)
     
     @bot.event
     async def on_ready():
         print(f'Logged in as {bot.user} (ID: {bot.user.id})')
         print('------')
+        try:
+            synced = await bot.tree.sync()
+            print(f'Synced {len(synced)} commands')
+        except Exception as e:
+            print(e)
 
     # /echo for debugging
-    @bot.command(name = 'echo', help = 'repeat entered text, for debugging')
-    async def echo(ctx, *, args):
-        await ctx.send(args)
+    @bot.tree.command(name = 'echo')
+    async def echo(interaction: discord.Interaction, message: str):
+        await interaction.response.send_message(message)
 
-    # /assignrole 
-    @bot.command(name = 'assignrole', help = '<member> <role to assign>')
-    async def assign_role(ctx, member: discord.Member, *, role: discord.Role):
+    # /assignrole <member> <role>
+    @bot.tree.command(name = 'assignrole')
+    # @app_commands.describe(...)
+    async def assign_role(interaction: discord.Interaction, member: discord.Member, *, role: discord.Role):
         await member.add_roles(role)
-        await ctx.send('Success!')
-    @assign_role.error #assignrole error checking
-    async def assign_role_error(ctx, error):
+        await interaction.response.send_message('Success!')
+    # /assignrole error checking
+    @assign_role.error
+    async def assign_role_error(interaction, error):
         if isinstance(error, commands.errors.MemberNotFound):
-            await ctx.send('That member does not exist. Typo?')
+            await interaction.response.send_message('That member does not exist. Typo?')
         if isinstance(error, commands.errors.RoleNotFound):
-            await ctx.send('That role does not exist. Typo?')
+            await interaction.response.send_message('That role does not exist. Typo?')
         else:
-            print(error)
+            await interaction.response.send_message(error)
         
     # Run the    
     bot.run(TOKEN)
