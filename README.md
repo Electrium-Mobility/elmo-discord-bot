e13e3ead96cec1d29628f7590dcb50614b15555f
J-L0115 <joelle.lee03@gmail.com>
2024-01-20 23:37:05 -0500

    changing joke api

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Electrium Mobility Discord Bot Developing...
 
 # Project Setup
 ![Alt text](image.png)
+
+modules to install:
+npm install discord.js
+npm install @octokit/rest
+npm install axios
+
+After adding/editing new commands, must run : 
+node deploy-commands.js
+then 
+node .
+
+setting up config.json
+
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-const axios = require('axios');
+//const axios = require('axios');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
