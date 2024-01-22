b29e0e2a23261e6cb878730ae5d05124ec48c8f8
J-L0115 <63077588+J-L0115@users.noreply.github.com>
2024-01-22 15:45:22 -0500

    removing comment in index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-//const axios = require('axios');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
