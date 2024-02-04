fd56f6c546fcdc66b40d6354ceb3da2051e7eb9b
JerryZhang0920 <jinxuan0920@gmail.com>
2024-02-04 17:34:25 -0500

    updated token

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits} = require('discord.js');
-const token = process.env.DISCORD_TOKEN;
+const token = process.env.TOKEN;
 const channelId = process.env.CHANNEL_ID;
 //const { token, channelId } = require('./config.json');
 const axios = require('axios');
