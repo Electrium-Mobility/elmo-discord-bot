1642cce3f36a76ef2b632231be54a9f5eaeb60a9
jenniferli8263 <75101197+jenniferli8263@users.noreply.github.com>
2024-02-11 12:02:13 -0500

    invite users on altium

---
+++ commands/utility/invite_altium.js
@@ -0,0 +1,71 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+const options = new chrome.Options();
+options.addArguments('--ignore-certificate-errors');
+options.addArguments('--ignore-ssl-errors');
+options.setAcceptInsecureCerts();
+
+async function inviteUser(email, password, invites) {
+    const driver = await new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
+    try {
+        driver.get("https://university-of-waterloo-38.365.altium.com/team/members");
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        //login
+        await driver.findElement(By.xpath('//*[@id="auth-module-page"]/div/main/div/div[2]/div/form/div[1]/div[2]/input')).sendKeys(email);
+        await driver.findElement(By.xpath('//*[@id="auth-module-page"]/div/main/div/div[2]/div/form/div[2]/div[2]/div/input')).sendKeys(password);
+        await driver.findElement(By.xpath('//*[@id="auth-module-page"]/div/main/div/div[2]/div/form/button')).click();
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        //invite
+        
+        // await driver.findElement(By.xpath('/html/body/div[10]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/input')).sendKeys(invites);
+
+        for(let i = 0; i < invites.length; i++){
+            await driver.findElement(By.xpath('//*[@id="app-team"]/div/div/div[2]/div[2]/div[3]/div[1]/div[1]/button')).click();
+            await driver.findElement(By.xpath('/html/body/div[10]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/input')).sendKeys(invites[i].email);
+            if(!invites[i].admin){
+                await driver.findElement(By.xpath('/html/body/div[10]/div/div[2]/div/form/div[2]/div/div[2]/div/div/div[1]/input')).clear();
+                await driver.findElement(By.xpath('/html/body/div[10]/div/div[2]/div/form/div[2]/div/div[2]/div/div/div[1]/input')).sendKeys('Engineers');
+            }
+            else{
+                await driver.findElement(By.xpath('/html/body/div[10]/div/div[2]/div/form/div[2]/div/div[2]/div/div/div[1]/input')).clear();
+                await driver.findElement(By.xpath('/html/body/div[10]/div/div[2]/div/form/div[2]/div/div[2]/div/div/div[1]/input')).sendKeys('Administrators');
+            }
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            await driver.findElement(By.xpath('/html/body/div[10]/div/div[2]/div/form/div[4]/div/button[1]')).click();
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            await driver.findElement(By.xpath('/html/body/div[10]/div/div[2]/div/form/div[4]/div/button[1]')).click();
+            
+            //confirmation button
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            await driver.findElement(By.xpath('/html/body/div[11]/div/div/div/div[2]/button[1]')).click();
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            
+        }
+
+
+    }
+    finally{
+        driver.quit();
+    }
+}
+
+const username = 'j444li@uwaterloo.ca';
+const password = 'Myor910v!';
+const test = "jenniferli8263@gmail.com"
+const emailsToInvite = [
+    {
+        email:'jenniferli8263@gmail.com',
+        admin: false
+    },
+    {
+        email:'yibangliu@gmail.com',
+        admin: true
+    }
+];
+
+inviteUser(username, password, emailsToInvite);
