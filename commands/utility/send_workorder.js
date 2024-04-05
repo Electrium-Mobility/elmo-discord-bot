dbea6d5f9607053b9649b5c63e746cdc9659edb2
George Li <georgeli293@gmail.com>
2024-04-05 19:34:35 -0400

    changed email dest for sendworkorder

--- commands/utility/send_workorder.js
+++ commands/utility/send_workorder.js
@@ -13,7 +13,7 @@ const REDIRECT_URL = 'http://localhost:3000/oauth2callback'; /* Page that user w
 const FILLED_FORMS_FOLDER = "1cBG-BgIbqXxvfmdNcgmw5WRST8jWE2Pn";
 
 /* EMAIL PARAMETERS - FEEL FREE TO CHANGE FOR YOUR SPECIFIC EMAIL NEEDS */
-const EMAIL_DEST = 'georgeli293@gmail.com'; /* Dest of the email E.g georgeli293@gmail.com  CHANGE LATER PLZ DONT SPAM MY EMAIL */
+const EMAIL_DEST = 'StudentDesignCentreEngineering@uwaterloo.ca';
 const EMAIL_SUBJECT = 'Electrium Mobility - Workorder - Team Funds';
 const EMAIL_BODY = 'Hi Sarah,\n\n Please see the attached work orders \n\n Thanks\n Julian Choi';
 
