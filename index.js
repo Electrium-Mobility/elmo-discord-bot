cba138fcdbc1a72aa744b1547897b8f7f21657b8
Nicole <nchung@live.com>
2024-04-28 20:13:56 -0700

    switched discord webhook id to the one on electrium server

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const { token, channelId } = require('./config.json');
 
 const { updateLocalTaskLists, getLocalTaskLists } = require('./helperFunctions/retrieve_task_lists');
 const { fetchSheetTitles } = require('./helperFunctions/google_sheet_helpers');
+
+const DISCORD_WEBHOOK_USER_ID = "1221912941255721211";
+// 1221908136554659900 nikki's test server webhook ID
+
 // Create a new client instance
 const client = new Client({
 	intents:
@@ -46,7 +50,7 @@ for (const folder of commandFolders) {
 // ping role when meeting starts
 client.on('messageCreate', async (message) => {
 	// if a message is coming from google cal webhook
-	if (message.author.id === "1221908136554659900") {
+	if (message.author.id === DISCORD_WEBHOO_USER_ID) {
 		try {
 			// get the role name from the message param
 			let roleName = message.embeds[0].data.fields[2].value;
@@ -75,7 +79,6 @@ client.on('messageCreate', async (message) => {
 
 //event listener
 client.on(Events.InteractionCreate, async interaction => {
-
 	if (!interaction.isChatInputCommand()) return;
 	
 	const command = interaction.client.commands.get(interaction.commandName);
